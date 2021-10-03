4d3c7bb3eb21b55af642d501e9f7bc7ac0720457
Johan Gustafsson <johan@tinyrocket.se>
2021-10-03 20:04:05 +0200

    distance blending for grass layer

---
+++ TR.Stride.Common/Hacks.cs
@@ -0,0 +1,27 @@
+﻿using Stride.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TR.Stride
+{
+    public static class Hacks
+    {
+        /// <summary>
+        /// Hack used to fix entities after hot reload
+        /// </summary>
+        public static TComponent RelinkComponent<TComponent>(EntityManager entityManager, Guid entityId)
+            where TComponent : EntityComponent
+        {
+            foreach (var entity in entityManager)
+            {
+                if (entity.Id == entityId)
+                {
+                    return entity.Get<TComponent>();
+                }
+            }
+
+            return default;
+        }
+    }
+}
