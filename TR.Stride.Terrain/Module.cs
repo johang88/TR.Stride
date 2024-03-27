07adb9cd342238a93a10d3859df018b9759afc10
Doprez <73259914+Doprez@users.noreply.github.com>
2024-03-27 07:23:01 -0600

    work around due to https://github.com/stride3d/stride/issues/2200

---
+++ TR.Stride.Terrain/Module.cs
@@ -0,0 +1,19 @@
+﻿using Stride.Core;
+using Stride.Core.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR.Stride.Terrain;
+internal class Module
+{
+	[ModuleInitializer]
+	public static void Initialize()
+	{
+		// Make sure that this assembly is registered
+		AssemblyRegistry.Register(typeof(Module).GetTypeInfo().Assembly, AssemblyCommonCategories.Assets);
+	}
+}
